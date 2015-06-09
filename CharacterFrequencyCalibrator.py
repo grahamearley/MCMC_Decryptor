3e37f70b46c0d388bf1068e4bcf350d407a34aea
Graham Earley <earleyg@carleton.edu>
2015-06-08 20:15:39 -0500

    Initial commit

---
+++ CharacterFrequencyCalibrator.py
@@ -0,0 +1,50 @@
+__author__ = 'grahamearley'
+
+
+class CharacterFrequencyCalibrator:
+    """
+    A class for storing the frequencies of character pairings,
+    calibrated to a specific text file (like a text from
+    Project Gutenberg).
+    """
+
+    def __init__(self, input_file_location):
+        self.input_text_file = open(input_file_location, 'r')
+        self.clean_text = self.calibrate_characters()
+        self.frequencies_dict = self.count_character_appearances()
+
+    def calibrate_characters(self):
+        non_punctuation_chars = []
+        text = self.input_text_file.read().lower()  # (Make all text lower case)
+
+        for char in text:
+            if char == "\n":
+                # Set line breaks as spaces.
+                non_punctuation_chars.append(" ")
+            elif char not in "`~!@#$%^&*()-_=+[{]}\|;:'\",<.>/?":
+                # Store it if it's not punctuation, etc.
+                non_punctuation_chars.append(char)
+
+        self.input_text_file.close()
+        non_punctuation_text = "".join(non_punctuation_chars)
+
+        return non_punctuation_text
+
+    def count_character_appearances(self):
+        frequencies_dict = {}
+        text_length = len(self.clean_text)-1
+
+        i = 0
+        while (i < text_length):
+            char_pair = self.clean_text[i] + self.clean_text[i+1]
+
+            if char_pair in frequencies_dict:
+                frequencies_dict[char_pair] += 1
+            else:
+                frequencies_dict[char_pair] = 1
+
+            i += 1
+
+        return frequencies_dict
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+__author__ = 'grahamearley'
+
+from CharacterFrequencyCalibrator import CharacterFrequencyCalibrator
+
+c = CharacterFrequencyCalibrator("PrideAndPrejudice.txt")
+print(c.frequencies_dict)
